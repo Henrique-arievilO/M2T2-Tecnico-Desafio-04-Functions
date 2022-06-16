4385ff3c93c64df641a5a5666540100db8de321d
Henrique Oliveira <wandholiveira1@gmail.com>
2022-06-16 20:02:26 -0300

    Exercícios sobre funções.

---
+++ script.js
@@ -0,0 +1,38 @@
+console.log("Exercício 01:")
+console.log("Qual é o sentido da vida?")
+function senseOfLife(numberCode){
+    return numberCode;
+}
+console.log("A resposta para tal complexidade existencial é simplesmente:", 42);
+
+console.log("Exercício 02:")
+function myName(name){
+    return name;
+}
+console.log('Wanderson Henrique Oliveira')
+
+console.log("Exercício 03:") //Darei 3 exemplos:
+function aboutMe(name, age, musicPreference){
+    return name, age, musicPreference;
+}
+console.log("Meu nome é","Henrique","eu tenho", "29 anos","e eu curto","Post-Punk!")
+console.log("Meu nome é","Micaele","eu tenho", "30 anos","e eu curto","Rock e Heavy Metal!")
+console.log("Meu nome é","Gabrielly","eu tenho", "22 anos","e eu curto","Hip-hop!")
+
+console.log("Exercício 04:")
+function popCulture(movie, music){
+    return movie, music;
+}
+console.log("Donnie Darko","/","Killing Moon")
+
+console.log("Exercício 05:")
+
+function tripleNumber(number){
+    return number * 3;
+}
+let reply1 = tripleNumber(3);
+console.log("O resultado é:", reply1)
+let reply2 = tripleNumber(6*8);
+console.log("O resultado é:", reply2)
+let reply3 = tripleNumber(10-8);
+console.log("O resultado é:", reply3)
